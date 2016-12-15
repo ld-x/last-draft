31f2b7802ccd49163dd8ec69e31f61f5adb7f6e1
StevenIseki <stevenisekimartin@gmail.com>
2016-12-15 15:18:19 +1100

    Fix up restructure

--- src/Final.js
+++ src/Final.js
@@ -1,29 +1,111 @@
 import React, { Component } from 'react'
-import Editor, { createEditorStateWithText } from './plugins/draft-js-plugins/draft-js-plugins-editor'
+import Editor, { createEditorStateWithText, composeDecorators } from './plugins/draft-js-plugins-editor'
 import { EditorState, convertFromRaw, convertToRaw, convertFromHTML, ContentState } from 'draft-js'
 import { stateToHTML } from 'draft-js-export-html'
-import { defaultSuggestionsFilter } from './plugins/draft-js-plugins/draft-js-mention-plugin'
 
-/* init the plugins */
-import { components, plugins, data, setupToolbars } from './plugins'
+/* Emoji plugin */
+import createEmojiPlugin from './plugins/draft-js-emoji-plugin'
+import emojiStyles from './styles/EmojiStyles.css'
+const emojiPlugin = createEmojiPlugin({
+  theme: emojiStyles
+})
+const { EmojiSuggestions } = emojiPlugin
 
-const { EmojiSuggestions, MentionSuggestions,
-  LinkAdd, ImageAdd, StickerSelect, Entry } = components
-const { focusPlugin, alignmentPlugin, resizeablePlugin, imagePlugin,
-  emojiPlugin, hashtagPlugin, linkifyPlugin,
-  mentionPlugin, stickerPlugin } = plugins
-const { mentions, stickers } = data
+/* Hashtag plugin */
+import createHashtagPlugin from './plugins/draft-js-hashtag-plugin'
+import hashtagStyles from './styles/HashtagStyles.css'
+const hashtagPlugin = createHashtagPlugin({
+  theme: hashtagStyles
+})
 
-let linkAddElement = null
-let inlineToolbarElement = null
-const addLink = () => { linkAddElement.openPopover() }
+/* Image with Alignment, dnd, focus, resize plugin */
+import createImagePlugin from './plugins/draft-js-image-plugin'
+import createAlignmentPlugin from './plugins/draft-js-alignment-plugin'
+import createFocusPlugin from './plugins/draft-js-focus-plugin'
+import createResizeablePlugin from './plugins/draft-js-resizeable-plugin'
 
+import focusStyles from './styles/FocusStyles.css'
+const focusPlugin = createFocusPlugin({ theme: focusStyles })
+const resizeablePlugin = createResizeablePlugin()
+import alignmentStyles from './styles/AlignmentStyles.css'
+const alignmentPlugin = createAlignmentPlugin({ theme: alignmentStyles })
+const { AlignmentTool } = alignmentPlugin
+
+/* alignmentPlugin.decorator, TODO: Needs theming */
+const decorator = composeDecorators(
+  resizeablePlugin.decorator,
+  focusPlugin.decorator,
+)
+const imagePlugin = createImagePlugin({ decorator })
+const { ImageAdd } = imagePlugin
 let imageAddElement = null
 const addImageFile = () => { imageAddElement.addImageFile() }
 
-const { inlineToolbarPlugin, InlineToolbar,
-  sideToolbarPlugin, SideToolbar } = setupToolbars(addLink, addImageFile)
+/* inline toolbar */
+import createInlineToolbarPlugin from './plugins/draft-js-inline-toolbar-plugin'
+import inlineToolbarStyles from './styles/inlineToolbarStyles.css'
+import inlineToolbarButtonStyles from './styles/InlineToolbarButtonStyles.css'
+import {
+  ItalicButton, BoldButton, UnderlineButton,
+  CodeButton, HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton,
+  UnorderedListButton, BlockquoteButton, AddLinkButton
+} from './plugins/draft-js-buttons/src/'
+const addLink = () => { linkAddElement.openPopover() }
+let linkAddElement = null
+let inlineToolbarElement = null
+const inlineToolbarPlugin = createInlineToolbarPlugin({
+  structure: [
+    ItalicButton, BoldButton, UnderlineButton,
+    CodeButton, HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton,
+    UnorderedListButton, BlockquoteButton, AddLinkButton
+  ],
+  addLink,
+  theme: { buttonStyles: inlineToolbarButtonStyles, toolbarStyles: inlineToolbarStyles }
+})
 
+const { InlineToolbar } = inlineToolbarPlugin
+
+/* Linkify */
+import createLinkifyPlugin from './plugins/draft-js-linkify-plugin'
+import linkifyStyles from './styles/Linkify.css'
+const linkifyPlugin = createLinkifyPlugin({ theme: linkifyStyles })
+const { LinkAdd } = linkifyPlugin
+
+/* Mentions */
+import createMentionPlugin, { defaultSuggestionsFilter } from './plugins/draft-js-mention-plugin'
+import mentionsStyles from './styles/Mention.css'
+import mentions from './components/Mention/mentions'
+import { Entry, positionSuggestions} from './components/Mention/mentions'
+const mentionPlugin = createMentionPlugin({
+  mentions,
+  positionSuggestions,
+  theme: mentionsStyles
+})
+const { MentionSuggestions } = mentionPlugin
+
+/* Side Toolbar */
+import createSideToolbarPlugin from './plugins/draft-js-side-toolbar-plugin'
+import buttonStyles from './styles/ToolbarButtonStyles.css'
+import toolbarStyles from './styles/ToolbarStyles.css'
+import blockTypeSelectStyles from './styles/ToolbarBlockTypeSelectStyles.css'
+const sideToolbarPlugin = createSideToolbarPlugin({
+  theme: { buttonStyles, toolbarStyles, blockTypeSelectStyles },
+  addImageFile
+})
+const { SideToolbar } = sideToolbarPlugin
+
+/* Stickers */
+import createStickerPlugin from './plugins/draft-js-sticker-plugin'
+import stickers from './components/Sticker/stickers'
+import stickerStyles from './styles/StickerStyles.css'
+const stickerPlugin = createStickerPlugin({
+  stickers: stickers,
+  theme: stickerStyles
+})
+const { StickerSelect } = stickerPlugin
+
+
+/* init the plugins */
 let pluginList = {
   imagePlugin: imagePlugin,
   emojiPlugin: emojiPlugin,
@@ -35,6 +117,7 @@ let pluginList = {
   stickerPlugin: stickerPlugin,
   stickerPlugin: stickerPlugin,
 }
+
 
 /* init the state, either from raw, html or text */
 import { text } from './initialState/text'
@@ -59,7 +142,7 @@ export default class Final extends Component {
 
   state = {
     editorState: STATE,
-    suggestions: data.mentions
+    suggestions: mentions
   }
 
   onChange = (editorState) => {
@@ -83,8 +166,13 @@ export default class Final extends Component {
 
   onSearchChange = ({ value }) => {
     this.setState({
-      suggestions: defaultSuggestionsFilter(value, data.mentions),
+      suggestions: defaultSuggestionsFilter(value, mentions),
     })
+  }
+
+  customCountFunction(str) {
+    const wordArray = str.match(/\S+/g)
+    return wordArray ? wordArray.length : 0
   }
 
   getPlugins() {
@@ -177,7 +265,6 @@ export default class Final extends Component {
       <div>
         <div className='editor' onClick={this.focus}>
           <Editor
-            defaultKeyBindings={true}
             editorState={this.state.editorState}
             onChange={this.onChange}
             plugins={this.getPlugins()}
