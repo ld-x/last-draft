32c09caf936968c7d5393f29935a2c5c5039a9f5
StevenIseki <stevenisekimartin@gmail.com>
2017-01-03 09:54:22 +1100

    Fix pullquote and align button hovers

--- src/components/Buttons/Align.js
+++ src/components/Buttons/Align.js
@@ -3,8 +3,8 @@ import React from 'react'
 export class AlignLeft extends React.Component {
   render () {
     return (
-      <svg fill="#FFF" height="24" viewBox="0 0 24 24" width="24">
-        <path d="M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z" />
+      <svg height='24' viewBox='0 0 24 24' width='24'>
+        <path d='M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z' fill='currentColor' />
       </svg>
     )
   }
@@ -13,8 +13,8 @@ export class AlignLeft extends React.Component {
 export class AlignCenter extends React.Component {
   render () {
     return (
-      <svg fill="#FFF" height="24" viewBox="0 0 24 24" width="24">
-        <path d="M7 15v2h10v-2H7zm-4 6h18v-2H3v2zm0-8h18v-2H3v2zm4-6v2h10V7H7zM3 3v2h18V3H3z" />
+      <svg height='24' viewBox='0 0 24 24' width='24'>
+        <path d='M7 15v2h10v-2H7zm-4 6h18v-2H3v2zm0-8h18v-2H3v2zm4-6v2h10V7H7zM3 3v2h18V3H3z' fill='currentColor' />
       </svg>
     )
   }
@@ -23,8 +23,8 @@ export class AlignCenter extends React.Component {
 export class AlignRight extends React.Component {
   render () {
     return (
-      <svg fill="#FFF" height="24" viewBox="0 0 24 24" width="24">
-        <path d="M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z" />
+      <svg height='24' viewBox='0 0 24 24' width='24'>
+        <path d='M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z' fill='currentColor' />
       </svg>
     )
   }
--- src/components/Buttons/Pullquote.js
+++ src/components/Buttons/Pullquote.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default class extends React.Component {
   render () {
     return (
-      <svg fill="#FFF" height="24" viewBox="0 0 24 24" width="24">
-        <defs><path d="M0 0h24v24H0V0z" id="a"/></defs>
-        <clipPath id="b"><use overflow="visible" />
-        </clipPath><path d="M4 9h16v2H4zm0 4h10v2H4z"/>
+      <svg height='24' viewBox='0 0 24 24' width='24' fill='currentColor'>
+        <defs><path d='M0 0h24v24H0V0z' id='a'/></defs>
+        <clipPath id='b'><use overflow='visible' /></clipPath>
+        <path d='M4 9h16v2H4zm0 4h10v2H4z'/>
       </svg>
     )
   }
