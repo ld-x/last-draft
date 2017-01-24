ff9d808273f8893677c76b00f526339bed1c189c
StevenIseki <stevenisekimartin@gmail.com>
2017-01-24 12:43:08 +1100

    Support atomic images

--- src/components/Blocks/Wrapper.js
+++ src/components/Blocks/Wrapper.js
@@ -1,17 +1,25 @@
+/*
+ * Copyright (c) 2016, vace.nz (https://github.com/vacenz)
+ *
+ * License: MIT
+ */
+
 import React, {Component} from 'react'
 
 export default class MyCustomBlock extends React.Component {
   render() {
     let block = this.props.children[0].props.children
     if (block !== undefined) {
-      const {plugin} = block.props.blockProps
-      if (plugin.wrapper !== undefined) {
-        let Wrapper = plugin.wrapper
-        return (
-          <Wrapper>
-            {this.props.children}
-          </Wrapper>
-        )
+      if (block.props.blockProps !== undefined) {
+        const {plugin} = block.props.blockProps
+        if (plugin.wrapper !== undefined) {
+          let Wrapper = plugin.wrapper
+          return (
+            <Wrapper>
+              {this.props.children}
+            </Wrapper>
+          )
+        }
       }
     }
 
